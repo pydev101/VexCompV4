66d17f81ef5a0a681ecf12d5e4cd4c758f1e30cd
pydev101 <30803947+pydev101@users.noreply.github.com>
2021-02-18 20:36:02 -0600

    Minor

--- CompV5/src/Learning.h
+++ CompV5/src/Learning.h
@@ -72,10 +72,12 @@ void learn(double e, double d, double t, Trial currentTrial){
 
 }
 
-void test(){
+void log(){
   //WORKS!
   vexFileMountSD();
   FIL* t = vexFileOpenCreate("Testing.txt");
+  FIL* f = vexFileOpen("Log.txt", "a");
+
   char b[] = {'H','e','l','l','o','\0'};
   vexFileWrite(b, sizeof(b), 1, t);
   vexFileClose(t);
