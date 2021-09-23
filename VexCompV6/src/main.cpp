b2de300c5fbf9b0749a51c5a00eac1d7cfe499f9
Carson E <30803947+pydev101@users.noreply.github.com>
2021-09-23 12:10:02 -0500

    Added v6

---
+++ VexCompV6/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\carson.easterling                                */
+/*    Created:      Thu Sep 23 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
---
+++ VexCompV6/src/odom/robotmath.h
@@ -0,0 +1,23 @@
+#ifndef __ROBOTMATH_H__
+#define __ROBOTMATH_H__
+#include "math.h"
+
+int abs())
+
+
+
+
+
+#endif
+
+/*
+int test(){
+    return 42;
+}
+
+int main(){
+    int (*p)() = test;
+    std::cout << test() << std::endl;
+    std::cout << p() << std::endl;
+    return 0;
+}*/
